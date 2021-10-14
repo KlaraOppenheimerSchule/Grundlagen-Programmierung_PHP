65bc2cc223a7cfa8f7b428bf447cfdaf1ec8900c
Pascal <Bayer.Pascalpb@gmx.de>
2021-10-14 09:47:08 +0200

    Aufgabe Mathefunktionen Pascal Bayer

---
+++ 01_Einfuehrung_OOP/01_Einfuehrung/Aufgabea.php
@@ -0,0 +1,97 @@
+<!doctype HTML>
+<html lang="">
+
+<head>
+    <title>Aufgabe Seite 16</title>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+
+<form method="post" action="<?php echo$_SERVER["PHP_SELF"]; ?>">
+    <label><input type="text" name="Zahl[]" /></label>
+    <label><input type="text" name="Zahl[]"  /></label>
+    </br>
+    <input type="submit"/>
+    <a href="Aufgabea.php"><label>Zurücksetzen</label></a>
+</form>
+<?php
+if(empty($_POST["Zahl"])){
+    echo "Bitte eine Eingabe tätigen!";
+}
+else {
+    $Zahlen = $_POST["Zahl"];
+
+    class Aufgaben
+    {
+        private $Zahl1;
+        private $Zahl2;
+
+        public function __construct($Zahl1, $Zahl2)
+        {
+            $this->Zahl1 = $Zahl1;
+            $this->Zahl2 = $Zahl2;
+        }
+
+        public function addition()
+        {
+            return $this->Zahl1 + $this->Zahl2;
+        }
+
+        public function quadrat()
+        {
+            return pow($this->Zahl1, 2);
+        }
+
+        public function Pruefen()
+        {
+            if ($this->Zahl1 < 0 || $this->Zahl2 < 0) {
+                $a = false;
+            } else {
+                $a = true;
+            }
+            return var_dump($a);
+        }
+
+        public function Fakultaet()
+        {
+            $fakul = 1;
+            for ($i = 1; $i <= $this->Zahl1; $i++) {
+                $fakul *= $i;
+            }
+            return $fakul;
+        }
+    }
+
+    $Loesung = new Aufgaben($Zahlen[0], $Zahlen[1]);
+    echo "Die Zahl bei Aufgabe a ist: " . $Loesung->addition();
+    echo "</br>";
+    echo "Die Zahl bei Aufgabe b ist: " . $Loesung->quadrat();
+    echo "</br>";
+    echo "Der bool ist: ";
+    $Loesung->Pruefen();
+    echo "</br>";
+    echo "Die Fakultät ist: " . $Loesung->Fakultaet();
+
+}
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+</html>
