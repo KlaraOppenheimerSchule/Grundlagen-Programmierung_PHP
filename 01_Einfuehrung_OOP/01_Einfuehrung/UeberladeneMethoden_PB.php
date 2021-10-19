f79e35e3f1f08ceb91a7ee58c511a3fff778ccbe
Pascal <Bayer.Pascalpb@gmx.de>
2021-10-19 11:44:23 +0200

    FunctionOverloading Bayer Pascal

--- 01_Einfuehrung_OOP/01_Einfuehrung/UeberladeneMethoden_PB.php
+++ 01_Einfuehrung_OOP/01_Einfuehrung/UeberladeneMethoden_PB.php
@@ -2,16 +2,10 @@
 
  class UeberladeneMethoden
     {
-        private $Zahl1;
-        private $Zahl2;
-        private $Zahl3;
 
-        public function __construct($Zahl1, $Zahl2, $Zahl3)
-        {
-            $this->Zahl1 = $Zahl1;
-            $this->Zahl2 = $Zahl2;
-            $this->Zahl3 = $Zahl3;
-        }
+
+
+
 
         public function Addition($eins, $zwei)
         {
@@ -30,7 +24,8 @@
 
  }
 
-
+    $test = new UeberladeneMethoden();
+    echo $test->Addition(2, 4);
 
 
 
